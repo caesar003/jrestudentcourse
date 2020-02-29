406be0909652c7162a49598d03d7e00a3e88fe79
caesar003 <caesarmuksid@gmail.com>
2020-02-29 12:45:40 +0800

    reverting mess

--- application/views/inc/scripts.php
+++ application/views/inc/scripts.php
@@ -1,5 +1,5 @@
 <!--script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous">
-</script> <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-dateFormat/1.0/jquery.dateFormat.min.js"></script>  
+</script> <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-dateFormat/1.0/jquery.dateFormat.min.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
@@ -14,3 +14,7 @@
 <script src="<?php echo base_url().'assets/js/tooltip-viewport.js'?>"></script>
 <script src="<?php echo base_url().'assets/js/jquery.form.min.js'?>"></script>
 <script src="<?php echo base_url().'assets/js/jquery-textrange.js'?>"></script>
+<script src="<?php echo base_url('assets/js/md/vendor/modernizer-2.6.2.min.js');?>"></script>
+<script src="<?php echo base_url('aseets/js/md/plugins.js');?>"></script>
+<script src="<?php echo base_url('assets/js/md/vendor/showdown.js');?>"></script>
+<script src="<?php echo base_url('assets/js/md/mdhtmlform.js');?>"></script>
