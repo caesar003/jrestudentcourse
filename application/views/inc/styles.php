46923efb36c05fe63d815205a0b34d6a2dd34711
caesar003 <caesarmuksid@gmail.com>
2020-03-02 14:58:32 +0800

    adding version number to the stylesheet link

--- application/views/inc/styles.php
+++ application/views/inc/styles.php
@@ -1,6 +1,6 @@
-       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> <META NAME="ROBOTS" CONTENT="NOINDEX, FOLLOW"> 
+       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> <META NAME="ROBOTS" CONTENT="NOINDEX, FOLLOW">
       <title><?php echo $title;?></title>
-     
+
       <!--link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.4.1/cosmo/bootstrap.min.css"-->
      <!--link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css');?>"-->
          <link rel="stylesheet" href="<?php echo base_url('assets/css/cosmo-bootstrap.min.css');?>">
@@ -8,11 +8,10 @@
         <link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/fontawesome-5.12.0/css/brands.css'?>">
         <link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/fontawesome-5.12.0/css/regular.css'?>">
         <link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/fontawesome-5.12.0/css/solid.css'?>"-->
-         <link rel="stylesheet" href="<?php echo base_url('assets/fontawesome-5.12.0/css/all.min.css');?>"> 
-     
+         <link rel="stylesheet" href="<?php echo base_url('assets/fontawesome-5.12.0/css/all.min.css');?>">
+
       <link rel="stylesheet" href="<?php echo base_url('assets/css/dataTables.bootstrap4.min.css');?>">
       <!--link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css"-->
-      
-      <link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/css/style.css'?>">
+
+      <link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/css/style.css?v=1.1'?>">
       <link rel="shortcut icon" type="image/x-icon" href="<?php echo base_url().'favicon.ico'?>">
-      
